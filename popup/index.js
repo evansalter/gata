2915ef1cd539ddb1e7235fbbf83823bf58881581
esalter-va <evanearlsalter@gmail.com>
2020-01-30 20:29:07 -0600

    Initial commit: empty popup

---
+++ popup/index.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import Popup from './popup.vue';
+
+new Vue({
+    render: h => h(Popup)
+}).$mount("#popup");
---
+++ popup/popup.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>Popup</h2>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+})
+</script>
+
+<style lang="scss">
+</style>
