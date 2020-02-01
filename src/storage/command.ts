df137797746efa21ead1eecd3fd0d0630b566fdf
esalter-va <evanearlsalter@gmail.com>
2020-02-01 13:23:37 -0600

    Convert to component classes; get save working

--- src/storage/command.ts
+++ src/storage/command.ts
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 export enum FieldType {
     String = 0,
 }
@@ -8,30 +10,57 @@ export class Field {
 }
 
 export class Command {
+    id: string;
     name: string;
     description?: string;
     url: string;
     fields: Field[];
 
-    constructor(name: string, url: string, fields: Field[], description?: string) {
+    created: string;
+
+    constructor();
+    constructor(url: string, fields: Field[], name?: string, description?: string);
+    constructor(url?: string, fields?: Field[], name?: string, description?: string) {
         this.name = name;
         this.url = url;
         this.fields = fields;
         this.description = description;
+
+        const now = moment();
+        this.id = now.utc().unix().toString();
+        this.created = now.toISOString();
     }
 
-    save(): void {
-        console.log(`setting ${JSON.stringify({[this.name]: this})}`);
-        debugger;
+    public static fromObject(obj: Object) {
+        const c = new Command();
+        for (let key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                c[key] = obj[key]
+            }
+        }
+        return c;
+    }
+
+    private validate() {
+        if (!this.url) {
+            throw new Error('URL is required');
+        }
+        if (!this.id) {
+            throw new Error('Internal Error: ID is required');
+        }
+    }
+
+    public save(): void {
+        this.validate()
         chrome.storage.sync.set({
-            [this.name]: this
+            [this.id]: this
         }, console.log);
     }
 
     public static list(): Promise<Command[]> {
         return new Promise<Command[]>((resolve, reject) => {
             chrome.storage.sync.get(results => {
-                resolve(Object.entries(results).map((v: [string, Command]) => v[1]))
+                resolve(Object.entries(results).map((v: [string, Command]) => Command.fromObject(v[1])))
             })
         });
     }
